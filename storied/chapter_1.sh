633f1a108b2c4c6d6a8393c9d6d3ad6a09133d33
FieryMewtwo <77066742+FieryMewtwo@users.noreply.github.com>
2021-09-29 07:38:34 -0400

    Create chapter_1.sh

---
+++ storied/chapter_1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Wha-?"
+echo "Who- who are you?"
+echo "Please, be someone else. Anyone else"
+read PERSON
+echo "$PERSON--!!"
+echo "No....."
+echo "No no no no no no no....... Please, no!"
+echo "You aren't welcome here! Leave! Before she finds you! Hurry!"
+echo "** You scurry away quickly... **"
+echo "##### COASTVIEW ######"
+echo "###### $PERSON's HOUSE ######"
+echo "###### 2F ######"
+sh ./chapter_2.sh
