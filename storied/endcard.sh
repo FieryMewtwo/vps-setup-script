06039ab44d22a2f8b9cf4af264bbcddc21cc595c
FieryMewtwo <77066742+FieryMewtwo@users.noreply.github.com>
2021-09-29 15:21:04 -0400

    Create endcard.sh

---
+++ storied/endcard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Thanks for playing silent town beta 1.0.1. This release was necessary to make sure that the scripts were terminated properly"
+echo "(beta 1.0.0 wouldn't even execute properly, as in i suck at shell scripting). I'm planning a few changes to some internal stuff,"
+echo "We can talk about it on the shiny new IRC channel in the repo's readme file."
+echo "############################################################################################################################"
+echo "Changes I'm planning:"
+echo "1. Find someone to rewrite the whole game in C++ or C#(when it's finished)"
+echo "2. Port to JavaScript and HTML5 so we can play silent town in a browser."
+echo "3. Release the game on itch.io (and possibly GOG/Steam?)"
+echo "4. Windows NT/Windows 9x/MS-DOS/DR-DOS/OS X for PowerPC Macs/macOS/macOS on ARM/Classic Mac OS/ ports(in the far future)"
+echo "############################################################################################################################"
+echo "####################################### THANKS FOR PLAYING #################################################################"
+echo "############################################################################################################################"
