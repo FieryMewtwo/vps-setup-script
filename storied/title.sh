8fb33f42385443199458deee90d6de39cf691873
FieryMewtwo <77066742+FieryMewtwo@users.noreply.github.com>
2021-09-29 07:44:04 -0400

    Create title.sh

---
+++ storied/title.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "silent tale"
+echo "release ver 1.0.1"
+echo "scripted by FieryMewtwo on GitHub"
+echo "Official silent tale repo:"
+echo "https://github.com/vps-setup-script/blob/main/storied/"
+echo "Game licensed under the silent tale License"
+echo "found in the repo"
+echo "website: fierymewtwo.github.io/silent-tale/home/"
+sh ./chapter_1.sh
